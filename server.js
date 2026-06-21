6c102cbe6379afb956ab6bc75166f1afad3b63fe
cihanozturk92 <cihanozturk92@gmail.com>
2026-06-21 22:48:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -63,7 +63,12 @@ app.get('/sitemap.xml', async (req, res) => {
 
         const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls.map(url => `<url><loc>${url}</loc><changefreq>weekly</changefreq><priority>0.8</priority></url>`).join('\n')}
+${urls.map(url => `
+<url>
+    <loc>${url}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+</url>`).join('')}
 </urlset>`;
 
         res.set('Content-Type', 'application/xml');
